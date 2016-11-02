c23ec895ce2d19b60442b61d8c60c7aa56f67685
Radu Dita <hades2510@yahoo.com>
2016-11-02 17:11:16 +0200

    added button for adding images and implemented action sheet and image picker

--- PhotoFeed/FeedViewController.swift
+++ PhotoFeed/FeedViewController.swift
@@ -19,4 +19,26 @@ class FeedViewController:UIViewController
         title = firKey.substring(to: firKey.index(firKey.startIndex, offsetBy:8))
     }
     
+    @IBAction func addPhoto(_ sender: UIBarButtonItem) {
+        let alert = UIAlertController(title: "Add photo to feed", message: "", preferredStyle: .actionSheet )
+        
+        let cameraAction = UIAlertAction(title: "Camera", style: .default){ action in self.showPicker(.camera)}
+        let galleryAction = UIAlertAction(title: "Photo", style: .default){ action in self.showPicker(.photoLibrary)}
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel){action in }
+        
+        alert.addAction(cameraAction)
+        alert.addAction(galleryAction)
+        alert.addAction(cancelAction)
+        
+        present( alert, animated:true )
+    }
+    
+    fileprivate func showPicker(_ type: UIImagePickerControllerSourceType )
+    {
+        let imagePicker = UIImagePickerController()
+        
+        imagePicker.sourceType = type
+        
+        present( imagePicker, animated:true )
+    }
 }

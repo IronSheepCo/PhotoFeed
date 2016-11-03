a8d3f026ffb9f63a2b32f2ba67140aa385ad0772
Radu Dita <hades2510@yahoo.com>
2016-11-03 17:21:56 +0200

    added custom cell

---
+++ PhotoFeed/DataCellView.swift
@@ -0,0 +1,15 @@
+//
+//  DataCellView.swift
+//  PhotoFeed
+//
+//  Created by Mac on 03/11/2016.
+//  Copyright © 2016 ironsheep.tech. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class DataCellView: UICollectionViewCell{
+    
+    @IBOutlet weak var imageView: UIImageView!
+}
